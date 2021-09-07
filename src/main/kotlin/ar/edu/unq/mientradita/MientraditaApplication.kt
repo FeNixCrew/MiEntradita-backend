c2bdea6d08defcdc4b9b440373da5ba5bb7bc592
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-09-07 16:21:25 -0300

    first commit

---
+++ src/main/kotlin/ar/edu/unq/mientradita/MientraditaApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.mientradita
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MientraditaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MientraditaApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/mientradita/MientraditaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.mientradita
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MientraditaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
