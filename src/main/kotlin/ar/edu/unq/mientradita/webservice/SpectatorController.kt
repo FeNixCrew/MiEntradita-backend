b70e2de5d2a0936a7f555cad77804eb96fb587a3
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-09-29 02:07:32 -0300

    :recycle: encapsulo login y register en controlador especifico

--- src/main/kotlin/ar/edu/unq/mientradita/webservice/SpectatorController.kt
+++ src/main/kotlin/ar/edu/unq/mientradita/webservice/SpectatorController.kt
@@ -1,40 +1,19 @@
 package ar.edu.unq.mientradita.webservice
 
-import ar.edu.unq.mientradita.model.user.Spectator
 import ar.edu.unq.mientradita.model.exception.MiEntraditaException
-import ar.edu.unq.mientradita.service.AuthUserService
 import ar.edu.unq.mientradita.service.SpectatorService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/spectator")
 class SpectatorController {
     @Autowired
-    private lateinit var authUserService: AuthUserService
-
-    @Autowired
     private lateinit var spectatorService: SpectatorService
-
-    @RequestMapping(value = ["/login"], method = [RequestMethod.POST])
-    fun logIn(@RequestBody loginRequest: LoginRequest): ResponseEntity<*> {
-        return try {
-            ResponseEntity.ok(authUserService.login(loginRequest))
-        } catch (exception: MiEntraditaException) {
-            ResponseEntity.badRequest().body(exception.toMap())
-        }
-    }
-
-    @RequestMapping(value = ["/register"], method = [RequestMethod.POST])
-    fun register(@RequestBody registerRequest: RegisterRequest): ResponseEntity<*> {
-        return try {
-            ResponseEntity(authUserService.createSpectator(registerRequest), HttpStatus.CREATED)
-        } catch (exception: MiEntraditaException) {
-            ResponseEntity.badRequest().body(exception.toMap())
-        }
-    }
 
     @RequestMapping(value = ["/tickets"], method = [RequestMethod.GET])
     fun pendingTicketsFrom(@RequestParam spectatorId: Long): ResponseEntity<*> {
@@ -44,17 +23,4 @@ class SpectatorController {
             ResponseEntity.badRequest().body(exception.toMap())
         }
     }
-}
-
-
-data class LoginRequest(val username: String, val password: String)
-
-data class RegisterRequest(val name: String,
-                           val surname: String,
-                           val username: String,
-                           val password: String,
-                           val dni: Int,
-                           val email: String
-) {
-    fun toModel() = Spectator(name, surname, username, email, dni, password)
 }
